e23c25706ff12f34cb9652553f7e188795637ce6
greatjing <greatjing@sina.cn>
2017-10-03 18:20:07 +0800

    user custom time format config

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,10 @@ module RailsRecipes
     config.i18n.default_locale = "zh-CN"
     # 设置默认时间为北京时间
     config.time_zone = "Beijing"
+
+    # 设置项目日常使用的格式(默认：default,自定义yml)
+    Time::DATE_FORMATS.merge!(:default => '%Y/%m/%d %I:%M %p', :ymd => '%Y/%m/%d')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
