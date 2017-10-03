6ffcd0a2421accbd7abadc1249cba9e7db663813
greatjing <greatjing@sina.cn>
2017-10-03 19:07:09 +0800

    use select to event status

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ApplicationRecord
  validates_presence_of :name, :friendly_id
 
  validates_uniqueness_of :friendly_id
- validates_format_of :friendly_id, :with => /\A[a-z0-9\_]+\z/
+ validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
 
 # 在新增的时候运行方法，生成friendly_id
  before_validation :generate_friendly_id, :on => :create
