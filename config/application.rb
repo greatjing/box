55d89b09700b9af79a84479a05fa1f37239cc405
greatjing <greatjing@sina.cn>
2017-10-03 18:02:33 +0800

    user custom time zone

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module RailsRecipes
 
     # 默认语系是中文
     config.i18n.default_locale = "zh-CN"
+    # 设置默认时间为北京时间
+    config.time_zone = "Beijing"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
