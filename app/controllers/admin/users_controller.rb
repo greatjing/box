ad5a1849665b4ccb2b748c0e9de0a3777face29a
greatjing <greatjing@sina.cn>
2017-10-04 15:59:27 +0800

    add admin/user index and edit

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,29 @@
+class Admin::UserController < AdminController
+
+  def index
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:notice] = "更新成功"
+      redirect_to admin_user_path
+    else
+      render "edit"
+    end
+  end
+
+  protected
+
+  def user_params
+    params.require[:user].permit(:email)
+  end
+
+
+end
