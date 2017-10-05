022a58dfb92c404ba2aeffb0dda102d83fefb1da
greatjing <greatjing@sina.cn>
2017-10-05 16:57:26 +0800

    input html can show safely

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,10 @@ module RailsRecipes
     # 设置默认时间为北京时间
     config.time_zone = "Beijing"
 
+    # 添加sanitize白名单元素（table，tr，td和style border）
+    config.action_view.sanitized_allowed_tags = Rails::Html::WhiteListSanitizer.allowed_tags + %w(table tr td)
+    config.action_view.sanitized_allowed_attributes = Rails::Html::WhiteListSanitizer.allowed_attributes + %w( style border )
+
     # 设置项目日常使用的格式(默认：default,自定义yml)
     Time::DATE_FORMATS.merge!(:default => '%Y/%m/%d %I:%M %p', :ymd => '%Y/%m/%d')
 
