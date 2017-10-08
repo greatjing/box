69753248a7a34fc7efbbd0184e959514cf63fcd6
greatjing <greatjing@sina.cn>
2017-10-08 23:42:40 +0800

    use date range filter registration

--- app/controllers/admin/event_registrations_controller.rb
+++ app/controllers/admin/event_registrations_controller.rb
@@ -22,6 +22,17 @@ class Admin::EventRegistrationsController < AdminController
     if Array(params[:ticket_ids]).any?
       @registrations = @registrations.by_ticket(params[:ticket_ids])
     end
+
+    # 按时间筛选
+    if params[:start_on].present?
+      @registrations = @registrations.where( " created_at >= ? ", Date.parse(params[:start_on]).beginning_of_day )
+    end
+
+    if params[:end_on].present?
+      @registrations = @registrations.where( " created_at <= ? ", Date.parse(params[:end_on]).end_of_day )
+      #@registrations = @registrations.where( "created_at <= ?", Date.parse(params[:end_on]).end_of_day )
+    end
+
   end
 
 
