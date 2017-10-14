942f57798d4cc18af4bd379fc0a253321c7c69e5
greatjing <greatjing@sina.cn>
2017-10-14 19:19:44 +0800

    show different ticket registration people count by status

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -19,14 +19,31 @@ class Admin::EventsController < AdminController
 
     ticket_names = @event.tickets.map{ |t| t.name }
 
+    status_colors = {"confirmed" => "#FF6384",
+                     "pending" => "#36A2EB"}
+
+# 不区分状态显示人数
+    # @data1 = {
+    #   labels: ticket_names,
+    #   datasets: [{
+    #     label: "# of Registrations",
+    #     data: @event.tickets.map{ |t| t.registrations.count },
+    #     backgroundColor: colors,
+    #     borderWidth: 1
+    #     }]
+    # }
+
+# 按状态显示人数
     @data1 = {
       labels: ticket_names,
-      datasets: [{
-        label: "# of Registrations",
-        data: @event.tickets.map{ |t| t.registrations.count },
-        backgroundColor: colors,
-        borderWidth: 1
-        }]
+      datasets: Registration::STATUS.map do |s|
+        {
+          label: I18n.t(s, :scope => "registration.status"),
+          data: @event.tickets.map{ |t| t.registrations.by_status(s).count },
+          backgroundColor: status_colors[s],
+          borderWidth: 1
+        }
+      end
     }
 
     @data2 = {
