3cf5eda3a49a20aa72d6b5c09792de8afd715c91
greatjing <greatjing@sina.cn>
2017-10-15 16:18:05 +0800

    editor user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+# 所有角色
+  ROLES = ["admin", "editor"]
 
 # user与group关系
   has_many :memberships
